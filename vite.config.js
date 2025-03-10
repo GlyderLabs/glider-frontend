6ee12640e59d9e1b6bd43dc2742d8365003fbab9
blok-hamster <josicoded@gmail.com>
2025-03-10 14:25:54 +0100

    enviroment varriables fixed

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'dotenv'
 
+config()
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 50390,
+    port: import.meta.env.VITE_PORT || 50390,
     host: '0.0.0.0',
     headers: {
       'Access-Control-Allow-Origin': '*',
